c8515bd5df2d6bfab53f2807afe0f6657128c03d
wyh <282943394@qq.com>
2023-04-06 17:29:49 +0800

    新增struct结构

---
+++ src/server.rs
@@ -0,0 +1,9 @@
+use super::router::Router;
+use http::httprequest::HttpRequest;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::str;
+
+pub struct Server<'a>{
+    socket_addr:&'a str,
+}
